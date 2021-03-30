591375124729a437393b0f70b88bfac4c828aed7
ahmedmeshref <a.meshref@alustudent.com>
2021-03-30 02:11:40 +0200

    Upload server file

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.get('/*', (req, res) => {
+ res.status(200).json({
+     success: true,
+     port: port 
+ });
+});
+
+
+app.listen(port, () => {
+ console.log(`Server running at port ${port}`);
+});
